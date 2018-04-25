c017ff76d23fa3482051a6cf9da403c4667ce80d
Alejandro de Tovar <venespana159@gmail.com>
2018-04-25 18:14:42 +0200

    create finder to unify all components to be used in search

---
+++ front-end/src/components/Finder.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <search
+            @submit="submitSearch"
+        />
+        <search-params
+            :last-search="lastSearch"
+            :total="total"
+            @updateParams="updateParams"
+        />
+    </div>
+</template>
+
+<script>
+import Search from './Search';
+import SearchParams from './SearchParams';
+
+export default {
+    name: 'finder',
+    props: {
+        total: {
+            type: Number,
+            default: 0,
+        },
+    },
+    data() {
+        return {
+            lastSearch: '',
+        };
+    },
+    methods: {
+        submitSearch({ current, last }) {
+            this.lastSearch = last;
+            this.$emit('submit', current);
+        },
+        updateParams(value) {
+            this.submitSearch({
+                current: '',
+                last: value,
+            });
+        },
+    },
+    components: {
+        Search,
+        SearchParams,
+    },
+};
+</script>
