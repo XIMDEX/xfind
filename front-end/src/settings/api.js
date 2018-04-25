3dbf76302ee5b1c823c66b5f4f30aa3ba6bde2e6
Alejandro de Tovar <venespana159@gmail.com>
2018-04-25 13:42:35 +0200

    created api controller to handle ajax

---
+++ front-end/src/settings/api.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { isNil } from 'ramda';
+import queryString from 'query-string';
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+axios.interceptors.response.use(
+    response => response.data,
+    error => Promise.reject(error.response.data)
+);
+
+export default function(Vue) {
+    Vue.xfind = {
+        get(uri, query = null, filters = {}) {
+            const configs = {
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                filters
+            };
+
+            if (!isNil(query) && query instanceof Object) {
+                query = queryString.stringify(query, { arrayFormat: 'index' });
+            }
+
+            if (!isNil(query) && typeof query === 'string') {
+                uri = `${uri}?${query}`;
+            }
+
+            return axios.get(uri, configs);
+        }
+    };
+
+    Object.defineProperties(Vue.prototype, {
+        $xfind: {
+            get: () => Vue.xfind
+        }
+    });
+}
