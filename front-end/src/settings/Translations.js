217f9305ac3c71b4d181abe1c34fc9e85fffffa1
Alejandro de Tovar <venespana159@gmail.com>
2018-07-12 11:34:52 +0200

    Added translations to front-end project

---
+++ front-end/src/settings/Translations.js
@@ -0,0 +1,16 @@
+module.exports = {
+    'es.resolutions': {
+        date: 'Fecha',
+        gravity_law: 'Ley gravedad',
+        type_procedure: 'Tipo de procedimiento',
+        activity_group: 'Grupo de actividad',
+        inflicted_item: 'Artículo infringido',
+        recurred: 'Recurrida',
+        author: 'Autor',
+        section: 'Sección',
+        state: 'Estado',
+        order: 'Ordenar',
+        new_results: 'Resultado más reciente',
+        old_results: 'Resultado menos reciente'
+    }
+};
