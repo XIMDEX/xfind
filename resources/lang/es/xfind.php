0487c143477e71324e3319df669ebbab0ab0adb3
Alejandro de Tovar <venespana159@gmail.com>
2019-03-06 18:16:49 +0100

    Added translations to models facets

---
+++ resources/lang/es/xfind.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'date' => 'Fecha',
+    'gravity_law' => 'Ley gravedad',
+    'type_procedure' => 'Tipo de procedimiento',
+    'activity_group' => 'Grupo de actividad',
+    'inflicted_item' => 'Artículo infringido',
+    'recurred' => 'Recurrida',
+    'author' => 'Autor',
+    'section' => 'Sección',
+    'state' => 'Estado',
+    'order' => 'Ordenar',
+    'new_results' => 'Resultado más reciente',
+    'old_results' => 'Resultado menos reciente',
+    'theme' => 'Tema',
+    'subtheme' => 'Sub tema',
+    'administration' => 'Administración',
+    'normative_range' => 'Rango normativo',
+    'year' => 'Fecha (año)',
+    'tags' => 'Etiquetas'
+];
