cbc20507dfbdbe36d2e80a42b0e11909bfdf79c3
Alejandro de Tovar <venespana159@gmail.com>
2019-04-10 13:56:47 +0200

    Clean content starts with ".", "Sección 1"

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -15,14 +15,25 @@ class Controller extends ItemController
             $title = array_key_exists('name', $doc) ? trim($doc['name']) : '';
             if (isset($doc['content_flat']) && !is_null($doc['content_flat'])) {
                 $content =  preg_replace('/\s+/', ' ', trim($doc['content_flat']));
+
+                if (starts_with($content, ".")) {
+                    $content = trim(substr($content, 1));
+                }
+
                 if (starts_with($content, $title)) {
                     $content = trim(substr($content, strlen($title)));
-                    if (starts_with($content, ".")) {
-                        $content = trim(substr($content, 1));
-                    }
-                    $doc['content_flat'] = $content;
-                    $doc['content'] = $content;
                 }
+
+                if (starts_with($content, 'Sección 1')) {
+                    $content = trim(substr($content, strlen('Sección 1')));
+                }
+
+                if (starts_with($content, ".")) {
+                    $content = trim(substr($content, 1));
+                }
+
+                $doc['content_flat'] = $content;
+                $doc['content'] = $content;
             }
         }
 
